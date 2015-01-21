c67b4a5db502195e7fa0782ea67e7b7165dbbc87
Mark Holtzhausen <Mark.Holtzhausen@media24.com>
2015-01-21 14:39:27 +0200

    Removed authcode

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var HipChat = require('./index');
 
 var hipchat = new HipChat({
-    'auth_token': '8a9cf80b45445999c5fef17c8c8615',
+    'auth_token': '<your auth token>',
     'room_id'   : 'TEST',
     'from'      : 'hipchat-message',
     title       : '<strong>Javascript Unit Tests</strong>',
